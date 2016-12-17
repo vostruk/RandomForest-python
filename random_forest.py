9e5571bce29e20729f42935c1ba189bbe0e3ae9d
Łukasz Wacławski <lwaclaws@gmail.com>
2016-12-17 20:05:31 +0100

    Real values

--- random_forest.py
+++ random_forest.py
@@ -31,8 +31,9 @@ class DecisionTree:
         self.max_tree_height = max_tree_height
         self.criterion = 'information gain ratio'
         self.categorical_features = [] if categorical_features is None else categorical_features
+        self.root = None
 
-    def _build_node(self, X, y):
+    def build_node(self, X, y):
         unique_y = np.unique(y)
         if len(unique_y) == 1:
             return LeafNode(unique_y[0])
@@ -41,8 +42,9 @@ class DecisionTree:
         selected_attributes = random.sample(range(all_attributes), self.max_attributes)
         best_improved_impurity_value = np.inf
         for attribute in selected_attributes:
-            for candidate_node in AttributeValueEqualNode.generate_candidate_nodes(X, y, attribute):
-                equal_labels, not_equal_labels = candidate_node.split_labels()
+            NodeType = AttributeValueEqualNode if attribute in self.categorical_features else AttributeValueLessNode
+            for candidate_node in NodeType.generate_candidate_nodes(X, y, attribute):
+                equal_labels, not_equal_labels = candidate_node.split_labels(X, y)
                 equal_impurity_value = self.impurity_metric(equal_labels)
                 not_equal_impurity_value = self.impurity_metric(not_equal_labels)
                 samples_num = len(y)
@@ -58,24 +60,81 @@ class DecisionTree:
 
         return best_node
 
-    def _build_tree(self, X, y):
-        root = self._build_node(X, y)
-        nodes_stack = [root]
+    def fit(self, X, y):
+        self.root = self.build_node(X, y)
+        nodes_stack = [(self.root, X, y)]
         while nodes_stack:
-            node = nodes_stack.pop()
-            non_leaf_children = [child for child in node.build_children(self) if not child.is_leaf()]
+            node, node_X, node_y = nodes_stack.pop()
+            non_leaf_children = [child for child in node.build_children(self, node_X, node_y) if not child[0].is_leaf()]
             nodes_stack.extend(non_leaf_children)
-        return root
+
+    def predict(self, X):
+        predicted_labels_generator = (self.root.predict(x) for x in X)
+        return np.fromiter(predicted_labels_generator, np.int)
 
 
 @attr.s
-class AttributeValueEqualNode:
+class NonLeafNode:
     attribute = attr.ib()
     value = attr.ib()
-    X = attr.ib()
-    y = attr.ib()
-    equal_subtree = attr.ib(default=None)
-    not_equal_subtree = attr.ib(default=None)
+    left_subtree = attr.ib(default=None)
+    right_subtree = attr.ib(default=None)
+
+    def split_labels(self, X, y):
+        left_subtree_indexes = self._get_left_subtree_indexes(X)
+        left_subtree_labels = y[left_subtree_indexes]
+        right_subtree_labels = y[np.invert(left_subtree_indexes)]
+        return left_subtree_labels, right_subtree_labels
+
+    def build_children(self, tree, X, y):
+        left_subtree_indexes = X[:, self.attribute] < self.value
+        right_subtree_indexes = np.invert(left_subtree_indexes)
+        left_subtree_X = X[left_subtree_indexes]
+        right_subtree_X = X[right_subtree_indexes]
+        left_subtree_y = y[left_subtree_indexes]
+        right_subtree_y = y[right_subtree_indexes]
+        left_subtree_node = tree.build_node(left_subtree_X, left_subtree_y)
+        right_subtree_node = tree.build_node(right_subtree_X, right_subtree_y)
+        self.left_subtree = left_subtree_node
+        self.right_subtree = right_subtree_node
+        return (
+            (left_subtree_node, left_subtree_X, left_subtree_y),
+            (right_subtree_node, right_subtree_X, right_subtree_y)
+        )
+
+    @staticmethod
+    def is_leaf():
+        return False
+
+
+@attr.s
+class AttributeValueLessNode(NonLeafNode):
+    def _get_left_subtree_indexes(self, X):
+        return X[:, self.attribute] < self.value
+
+    def predict(self, x):
+        if x[self.attribute] < self.value:
+            return self.left_subtree.predict(x)
+        return self.right_subtree.predict(x)
+
+    @classmethod
+    def generate_candidate_nodes(cls, X, y, attribute):
+        values = np.unique(X[:, attribute])
+        sorted_values = np.sort(values)
+        for lower, higher in zip(sorted_values, sorted_values[1:]):
+            split_point = (lower + higher) / 2.0
+            yield cls(attribute, split_point, X, y)
+
+
+@attr.s
+class AttributeValueEqualNode(NonLeafNode):
+    def _get_left_subtree_indexes(self, X):
+        return X[:, self.attribute] == self.value
+
+    def predict(self, x):
+        if x[self.attribute] == self.value:
+            return self.left_subtree.predict(x)
+        return self.right_subtree.predict(x)
 
     @classmethod
     def generate_candidate_nodes(cls, X, y, attribute):
@@ -83,33 +142,14 @@ class AttributeValueEqualNode:
         for value in values:
             yield cls(attribute, value, X, y)
 
-    def split_labels(self):
-        equal_indexes = self.X[:, self.attribute] == self.value
-        equal_labels = self.y[equal_indexes]
-        not_equal_labels = self.y[np.invert(equal_indexes)]
-        return equal_labels, not_equal_labels
-
-    def build_children(self, tree):
-        equal_indexes = self.X[:, self.attribute] == self.value
-        equal_X = self.X[equal_indexes]
-        not_equal_X = self.X[np.invert(equal_indexes)]
-        equal_y = self.y[equal_indexes]
-        not_equal_y = self.y[np.invert(equal_indexes)]
-        equal_node = tree._build_node(equal_X, equal_y)
-        not_equal_node = tree._build_node(not_equal_X, not_equal_y)
-        self.equal_subtree = equal_node
-        self.not_equal_subtree = not_equal_node
-        self.X = None
-        self.y = None
-        return equal_node, not_equal_node
-
-    def is_leaf(self):
-        return False
-
 
 @attr.s
 class LeafNode:
     class_ = attr.ib()
 
-    def is_leaf(self):
+    def predict(self, x):
+        return self.class_
+
+    @staticmethod
+    def is_leaf():
         return True
